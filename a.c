c66e7ccd14b1779c550eac93cd9bd3b1f2e60fb4
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-10-30 11:52:06 +0300

    Update a.c

--- a.c
+++ a.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 int main()
 {
-    printf("Input size\n");
-    int a,b,s;
-    scanf("%d%d",&a,&b);
-    int arr[a][b];
-    printf("Input skaliar\n");
-    scanf("%d",&s);
-    printf("Input matricu\n");
-    for (int i=0; i<a; i++)
-    {
-        for (int j=0; j<b; j++)
-        {
-            scanf("%d", &arr[i][j]);
-            arr[i][j]*=s;
-        }
-    }
-    for (int i=0; i<a; i++)
-    {
-        for (int j=0; j<b; j++)
-        {
-            printf("%d ",arr[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
+	int s;
+	const int a = 2;
+	const int b = 2;
+	int arr[a][b];
+	printf("Input skaliar\n");
+	scanf("%d", &s);
+	printf("Input matricu\n");
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			scanf("%d", &arr[i][j]);
+			arr[i][j] *= s;
+		}
+	}
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
 }
